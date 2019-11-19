84a19a54f381b9a1356da03744ce7377188b7995
Jonad Dhrami <jdhrami@guidewire.com>
2019-11-19 14:54:52 +0000

    First Commit

---
+++ my-blog-backend/src/build/precache-manifest.9fa52fd73ee2114e172cbf9812cd6529.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a22c09f821fca4ea7e34d6399b489c06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa229cf95124bef3152e",
+    "url": "/static/css/main.01f8efe7.chunk.css"
+  },
+  {
+    "revision": "9f00678fafca56c648fe",
+    "url": "/static/js/2.ffd36cdd.chunk.js"
+  },
+  {
+    "revision": "fa229cf95124bef3152e",
+    "url": "/static/js/main.90bfe223.chunk.js"
+  },
+  {
+    "revision": "19dffa7a1e78686fab4a",
+    "url": "/static/js/runtime-main.ca086102.js"
+  }
+]);
